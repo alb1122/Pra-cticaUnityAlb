e56ac51ffae35840efa20f9cb5d82543e10dbc06
alb1122 <alb1122oli@gmail.com>
2015-05-11 17:45:10 +0200

    añadido camvas de bomba, sin script, a revisar el lanzamiento de bombas

--- SpaceshipShooter/Assets/controlBomba.cs
+++ SpaceshipShooter/Assets/controlBomba.cs
@@ -1,10 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class controlBomba : MonoBehaviour {
 
 	// Use this for initialization
+	public int numBombas = 3;
+	public GameObject MarcadorBombas;
+	public Sprite bomSprite;
+	public Sprite[] sprites;
+
+
 	void Start () {
+		    
+
+			
+			
+				for (int i = 0; i < numBombas; i++)
+				{
+					//GameObject MarcadorBombas = new GameObject("UI Element " + i.ToString());
+
+					// Image imagen = MarcadorBombas.AddComponent(typeof(Image));
+					
+					//imagen.sprite = sprites[Random.Range(0, sprites.Length)];
+					//MarcadorBombas.transform.SetParent(gameObject, false);
+				}
+
+
+
 	
 	}
 	
