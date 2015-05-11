25ac344da79056107e05c78f0adb205be7a7a2a1
alb1122 <alb1122oli@gmail.com>
2015-05-11 21:59:14 +0200

    añadido explosiones

--- SpaceshipShooter/Assets/ControlAsteroide.cs
+++ SpaceshipShooter/Assets/ControlAsteroide.cs
@@ -4,14 +4,18 @@ using System.Collections;
 public class ControlAsteroide : MonoBehaviour
 {
 	public GameObject marcador;
-
+	public GameObject nave;
 	// Por defecto, 100 puntos
 	public int puntos = 100;
+	public GameObject exp;
+	public GameObject navExp;
 
 	// Localizar y conectar el marcador para poder actualizarlo
 	void Start ()
 	{
 		marcador = GameObject.Find ("Marcador");
+		nave = GameObject.Find ("nave");
+
 	}
 
 	// Detectar la colisión entre el asteroide y el disparo
@@ -22,13 +26,17 @@ public class ControlAsteroide : MonoBehaviour
 		if (coll.gameObject.tag == "disparo") {
 			// Sumar la puntuación de este asteroide
 			marcador.GetComponent<ControlMarcador> ().puntos += puntos;
+			Instantiate (exp, transform.position, transform.rotation);
 
 			// El disparo desaparece
+			//coll.gameObject.GetComponent<ParticleAnimator>()
 			coll.gameObject.GetComponent<Renderer>().enabled = false;
 			coll.gameObject.GetComponent<Collider2D>().enabled = false;
 		} else {
 			if (coll.gameObject.tag == "nave") {
 				// Hemos chocado con la nave, restamos una vida
+				Instantiate (navExp, transform.position, transform.rotation);
+				Destroy(nave);
 				if (marcador.GetComponent<ControlMarcador> ().vidas > 0) {
 					marcador.GetComponent<ControlMarcador> ().vidas -= 1;
 				}
